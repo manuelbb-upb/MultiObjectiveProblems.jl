dcf1a1c9ee27683765f161f50edf43e515225bf4
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2021-03-21 16:10:40 +0100

    moving to github I

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,29 @@
+using Pkg
+Pkg.activate(@__DIR__)
+#%%
+using MultiObjectiveProblems
 using Documenter
 
-push!(LOAD_PATH, joinpath(@__DIR__,  "../src/") )
-using MultiObjectiveProblems
+DocMeta.setdocmeta!(MultiObjectiveProblems, :DocTestSetup, :(using MultiObjectiveProblems); recursive=true)
 
-makedocs(sitename="MultiObjectiveProblems Documentation")
+makedocs(;
+    modules=[MultiObjectiveProblems],
+    authors="Manuel Berkemeier",
+    repo="https://github.com/manuelbb-upb/MultiObjectiveProblems.jl/blob/{commit}{path}#{line}",
+    sitename="MultiObjectiveProblems.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://manuelbb-upb.github.io/MultiObjectiveProblems.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "DTLZ Problems" => "dtlz.md",
+        "ZDT Problems" => "zdt.md",
+        "MHT Problems" => "mht.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/manuelbb-upb/MultiObjectiveProblems.jl",
+)
